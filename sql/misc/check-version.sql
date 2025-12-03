609f6030582865e32a0b2b20d933bf6d0e961345
Kieran Pritchard <kierock03@gmail.com>
2025-12-03 18:24:55 +0000

    feat: version checking script

---
+++ sql/misc/check-version.sql
@@ -0,0 +1,7 @@
+-- Source - https://stackoverflow.com/a
+-- Posted by Thomas Kirchhoff, modified by community. See post 'Timeline' for change history
+-- Retrieved 2025-12-03, License - CC BY-SA 4.0
+
+SELECT version
+FROM product_component_version
+WHERE product LIKE 'Oracle Database%';
