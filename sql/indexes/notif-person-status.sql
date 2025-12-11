90ecb39798db0164979837a000910411fc4f79f6
Kieran Pritchard <kierock03@gmail.com>
2025-12-11 10:15:09 +0000

    fix: update notification index

--- sql/indexes/notif-person-status.sql
+++ sql/indexes/notif-person-status.sql
@@ -1,2 +1,2 @@
--- Creates an index on the notifications table to optimise queries filtering by PersonID and Status
-CREATE INDEX idx_notif_person_status ON notifications (personid, status);
+-- Creates an index on the notifications table to optimise queries filtering by PersonID and IsRead
+CREATE INDEX idx_notif_person_isread ON notifications (personid, isread);
